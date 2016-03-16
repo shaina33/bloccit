4e0a7187e12770445a1cf0c82b10e0ef849e8b58
Shaina Karasin <skarasin39@gmail.com>
2016-03-16 21:32:14 +0000

    assignment 28: complete

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+    let(:question) { Question.create!(title: "New Question?", body: "New Question Body", resolved: false) }
+    let(:answer) {Answer.create!(body: "Answer Body", question: question)}
+    
+    describe "attributes" do
+        it "responds to body" do
+            expect(answer).to respond_to(:body)
+        end
+    end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+    let(:question) { Question.create!(title: "New Question?", body: "New Question Body", resolved: false) }
+ 
+    describe "attributes" do
+        it "responds to title" do
+            expect(question).to respond_to(:title)
+        end
+        it "responds to body" do
+            expect(question).to respond_to(:body)
+        end
+        it "responds to resolved" do
+            expect(question).to respond_to(:resolved)
+        end
+    end
+end
