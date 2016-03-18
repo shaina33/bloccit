1861e3d1877ee8ada49bd22daada86fbbd48b6e3
Shaina Karasin <skarasin39@gmail.com>
2016-03-18 18:31:49 +0000

    checkpoint 31: complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'posts/index'
+  #get 'posts/show'
+  #get 'posts/new'
+  #get 'posts/edit'
+  resources :posts
 
-  get 'welcome/about'
-  
-  get 'welcome/contact'
-  
-  get 'welcome/faq'
-
+  #get 'welcome/index'
   root 'welcome#index'
-  
+  #get 'welcome/about'
+  get 'about' => 'welcome#about'
+  #get 'welcome/contact'
+  get 'contact' => 'welcome#contact'
+  #get 'welcome/faq'
+  get 'faq' => 'welcome#faq'
+
 end
