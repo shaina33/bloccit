e9e77131dc1844bfa2001829dc9722dab1a0de27
Shaina Karasin <skarasin39@gmail.com>
2016-03-29 14:05:23 +0000

    assignment 37: complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,26 @@ class UsersController < ApplicationController
     end
     
     def create
-     @user = User.new
-     @user.name = params[:user][:name]
-     @user.email = params[:user][:email]
-     @user.password = params[:user][:password]
-     @user.password_confirmation = params[:user][:password_confirmation]
- 
-     if @user.save
-       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-       redirect_to root_path
-     else
-       flash.now[:alert] = "There was an error creating your account. Please try again."
-       render :new
-     end
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+        
+        if @user.save
+            flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "There was an error creating your account. Please try again."
+            render :new
+        end
+    end
+    
+    def confirm
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:new, :create]
+  
+  post 'users/confirm' => 'users#confirm'
 
   #get 'welcome/index'
   root 'welcome#index'
