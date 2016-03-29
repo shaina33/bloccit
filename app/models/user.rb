7d13b6d3b533f986ce55b082f19d37efc914cd32
Shaina Karasin <skarasin39@gmail.com>
2016-03-29 12:33:36 +0000

    assmt 36: updated, chkpt 37: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+    before_save { self.name = name.split(" ").map {|w| w.capitalize}.join(" ") }
     
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
